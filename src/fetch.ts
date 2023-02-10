55e9371b9187183e4658f3f7039a9bb78cf039a0
Aaron Sutton <hey@aaron.as>
2023-02-10 10:45:37 -0500

    fix: clone the input stream

--- src/fetch.ts
+++ src/fetch.ts
@@ -9,7 +9,9 @@ export function toFetchHandler(app: App): FetchHandler {
   return async function toFetchHandle(input: Request): Promise<Response> {
     try {
       const url = new $URL(input.url).fullpath;
-      const r = await call(new Request(url, input.clone()));
+			// Needed to tee the input stream, otherwise output is not readable. 
+      input.clone();
+      const r = await call(new Request(url, input));
       return new Response(r.body, {
         status: r.status,
         statusText: r.statusText,
